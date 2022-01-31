39772f7d48f6a10b803a520c96bb78af1b51f908
dion fananie <dionfananie@gmail.com>
2022-02-01 06:38:43 +0700

    mongo config

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ app.use(bodyParser.json())
 app.use('/post', postRoutes)
 
 // connect to DB
-mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true},()=>{
+
+const mongoUrl = `mongodb+srv://${process.env.MONGO_NAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/myFirstDatabase?retryWrites=true&w=majority`
+mongoose.connect(mongoUrl,{useNewUrlParser:true},()=>{
     console.log('connected to DB Mongo Atlas!');
 })
 
--- routes/post.js
+++ routes/post.js
@@ -2,27 +2,25 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+router.get('/', async (req, res)=>{
+  try {
+      const posts = await Post.find();
+      res.json(posts)
+  } catch (error) {
+      res.json({message: err})
+      
+  }
 
-router.get('/', (req, res)=>{
-    res.send('my post now');
-});
+})
 
-
-router.get('/spesific', (req, res)=>{
-    res.send('my spesific post now');
-});
-
-
-router.post('/', (req, res)=>{
+router.post('/', async (req, res)=>{
     const post = new Post({
         title: req.body.title,
         desc: req.body.desc
     })
-    post.save().then(data=>{
-        res.json(data)
-    }).catch(err=>{
-        res.json({message: err})
-    })
+    const savePost = await post.save();
+    res.json(savePost);
+
 })
 
 module.exports = router;
