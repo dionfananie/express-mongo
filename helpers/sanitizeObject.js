e2a60222bc036a5b27841a8dc663410097cbf914
dion fananie <dionfananie@gmail.com>
2022-07-19 18:25:33 +0700

    improvement projection api

--- helpers/sanitizeObject.js
+++ helpers/sanitizeObject.js
@@ -1,6 +1,7 @@
 const sanitizeMongo = require('mongo-sanitize');
 
 function sanitizeObject(projection) {
+  if (!projection) return null;
   const projectionObj = JSON.parse(projection);
   const trustedProjection = sanitizeMongo(projectionObj);
   return trustedProjection;
