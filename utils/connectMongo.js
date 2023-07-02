b4a1feae7d5ce5c9dfb47fbbb5c47f9b62b5619e
dion fananie <dionfananie@gmail.com>
2023-07-02 19:21:14 +0700

    change port text

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -4,7 +4,7 @@ function connectMongo() {
   const mongoUrl = `mongodb+srv://${process.env.MONGO_NAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
   mongoose.connect(mongoUrl, { useNewUrlParser: true }, () => {
     console.log('connected to DB Mongo Atlas!');
-    console.log('Running on port 3030.');
+    console.log(`Running on port ${process.env.PORT}`);
   });
 }
 
